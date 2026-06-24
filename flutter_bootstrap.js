2544623d7d5b8042f4e6c0004b0916491f28e33c
samuel catalan <samuelcatalanz123@gmail.comalanz123@gmail.com>
2026-06-24 09:46:20 -0600

    deploy: fix pantalla en blanco (arranque a prueba de fallos)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "342157376" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "703686509" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
